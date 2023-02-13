17c8b5b8cbdb9e8050d3c6d8cb1d977060ebc985
Mohitpatel9999 <47581807+Mohitpatel9999@users.noreply.github.com>
2023-02-13 13:31:19 -0600

    removed extra comments

--- thermostat.srcs/sources_1/new/thermostat.vhd
+++ thermostat.srcs/sources_1/new/thermostat.vhd
@@ -1,35 +1,5 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/01/2023 01:56:55 PM
--- Design Name: 
--- Module Name: thermostat - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity thermostat is
     Port ( current_temp : in STD_LOGIC_VECTOR (6 downto 0);
